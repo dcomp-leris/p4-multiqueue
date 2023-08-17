d6f4a382a9b234d8bcd7dacaec863abb0301eefd
MateusBragatto <87801034+MateusBragatto@users.noreply.github.com>
2023-08-17 18:48:51 -0300

    Delete labs directory

--- labs/labs/env.sh
+++
@@ -1,5 +0,0 @@
-
-THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-#esses caminhos nem fazem sentido, sao os antigos
-BMV2_PATH=/home/vagrant/behavioral-model
-P4C_BM_PATH=/home/vagrant/p4c
